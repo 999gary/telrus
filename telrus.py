e9ea40957e20612518191a995b94c12c08365de4
999gary <15350912+999gary@users.noreply.github.com>
2017-10-01 01:31:39 -0500

    added config and timer

--- telrus.py
+++ telrus.py
@@ -1,21 +1,27 @@
 import sys
 import pygame
 import math
+import os
+import ConfigParser as cp
 true = True;
+false = False; #xd
+
 
 class Telrus:
     def __init__(self):
         print( '[Telrus] Initalizing Main Class' );
         pygame.init();
         pygame.font.init();
-        self.width = 1000;
-        self.height = 800;
+        self.width = config.config.getint('Window', 'width');
+        self.height = config.config.getint('Window', 'height');
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.maintimer = pygame.time.Clock()
+
 
     def run(self):
         print( 'lmao this is so not finished' );
         while( true ):
-            'lmaos'
+            self.maintimer.tick(config.config.getint('Window', 'fps'))
 
 
 
@@ -36,7 +42,29 @@ class Player:
         self.name = name;
         self.id = id;
 
+class TelrusConfig:
+
+    def __init__(self):
+        print( '[Telrus] Config Loaded.');
+        if ~os.path.isfile('config.cfg'):
+            self.firstopen = true;
+
+        #TODO Make this process more streamlined
+        with open( 'config.cfg', 'wb+' ) as cfg:
+            self.configfile = cfg;
+        self.config = cp.RawConfigParser();
+        if self.firstopen:
+            self.config.add_section('Window');
+            self.config.set('Window', 'fps', '60');
+            self.config.set('Window', 'width', '1000');
+            self.config.set('Window', 'height', '800');
+            self.config.write(open( 'config.cfg', 'wb+' ));
+        else:
+            self.config.read(open( 'config.cfg', 'r'));
 
 
+
+
+config = TelrusConfig();
 main = Telrus();
 main.run();
