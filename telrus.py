06be9bb039b4627c319185f486e510bf924a2531
999gary <15350912+999gary@users.noreply.github.com>
2017-10-01 00:05:47 -0500

    fixing jtf98s amazing work and adding an actual intilization

--- telrus.py
+++ telrus.py
@@ -1,21 +1,34 @@
 import sys
 import pygame
+import math
 
 
 class Telrus:
     def __init__(self):
         print( '[Telrus] Initalizing Main Class' );
         pygame.init();
+        pygame.font.init();
+    def run():
+        print( 'lmao this is so not finished' );
 
 
 class TelrusGameInstance:
     #You thought you would get good, quality code from this project.
     #But instead you got me,
     #Dio
-    def __init__(self):
+    def __init__(self, player):
         print( '[Telrus] New Game Board Instance Thing' );
-        self.player = ''
+        self.player = player;
+
+    def newboard(self, rows, lanes):
+        self.rows = rows;
+        self.lanes = lanes;
 
 class Player:
-    def __init__(self):
-        self.name = ''
+    def __init__(self, name, id):
+        self.name = name;
+        self.id = id;
+
+
+
+mainclass = Telrus();
