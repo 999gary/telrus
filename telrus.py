7a340939632a95f7e889833c9598aaff3c878de6
999gary <15350912+999gary@users.noreply.github.com>
2017-09-30 22:47:08 -0500

    Added Instance Class

--- telrus.py
+++ telrus.py
@@ -6,3 +6,12 @@ class Telrus:
     def __init__(self):
         print( '[Telrus] Initalizing Main Class' );
         pygame.init();
+
+
+class TelrusGameInstance:
+    #You thought you would get good, quality code from this project.
+    #But instead you got me,
+    #Dio
+    def __init__(self):
+        print( '[Telrus] New Game Board Instance Thing' );
+        self.player = ''
